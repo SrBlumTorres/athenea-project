c1cc9d27fb2b10ddfb92fc59ec2eb7f046ff4e67
SrBlumTorres <ablumtorres7@gmail.com>
2025-06-29 20:00:25 +0200

    implementing navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,29 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { NewUserComponent } from './pages/new-user/new-user.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    title: 'Home'
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        title: 'Home'
+      },
+      {
+        path: 'profile/:id',
+        component: UserProfileComponent,
+        title: 'User Profile'
+      },
+      {
+        path: 'new-user',
+        component: NewUserComponent,
+        title: 'New User'
+      }
+    ]
   },
-  {
-    path: 'profile/:id',
-    component: UserProfileComponent,
-    title: 'User Profile'
-  },
-  {
-    path: 'new-user',
-    component: NewUserComponent,
-    title: 'New User'
-  },
-  {
-    path: '',
-    component: HomeComponent,
-    title: 'Home'
-  },
-  // In case of no match
   { path: "**", redirectTo: "/" }
 ];
