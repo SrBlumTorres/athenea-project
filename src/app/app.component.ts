21ac240e4d2c3737ca0185753cfaac3190c89a8a
SrBlumTorres <ablumtorres7@gmail.com>
2025-06-29 14:36:27 +0200

    Add routes to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="favicon.ico"
-          alt="logo"
-          aria-hidden="true"
-        />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="favicon.ico"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: 'profile:id',
+    component: UserProfileComponent,
+    title: 'User Profile'
+  },
+  {
+    path: 'new-user',
+    component: NewUserComponent,
+    title: 'New User'
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  // In case of no match
+  { path: "**", redirectTo: "/" }
+];
