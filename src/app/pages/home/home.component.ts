294ffc83248c78171e85d31f78b29b3b3c3c98f5
SrBlumTorres <ablumtorres7@gmail.com>
2025-06-29 12:12:01 +0200

    integration users with home, testing component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { UsersComponent } from '../users/users.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UsersComponent],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by user" />
+        <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-users></app-users>
     </section>
   `,
   styleUrl: './home.component.css',
---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  template: `Users works`,
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+
+}
